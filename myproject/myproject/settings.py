9d4a12f44bfaad4d70950a406a6fc608638b2f36
Ajay Kumar <ajay@careinsight.ai>
2025-02-27 19:07:20 +0530

    added requirements.txt file and verson.json

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'myproject.middleware.RestrictIPMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
